a6ae1da3b7499653c005a4b1d3d232268b74b226
LautaAndreani <lautaroandreani2016@gmail.com>
2022-09-19 20:21:35 -0300

    fix: button & content link :bug:

--- components/ButtonComp.tsx
+++ components/ButtonComp.tsx
@@ -4,14 +4,12 @@ type Props = { link: string; social: string }
 
 const ButtonComp = ({ link, social }: Props) => {
 	return (
-		<Button size="md" w="fit-content" bg="none" border="1px solid" borderColor="brand.border_color" gap={2} p="10px 20px" _hover={{}}>
-			<Image src={`/assets/icons/${social}.svg`} />
-			<NextLink href={link} passHref>
-				<Link isExternal textDecoration="none" fontWeight={400}>
-					{social}
-				</Link>
-			</NextLink>
-		</Button>
+		<Link isExternal href={link} fontWeight={400} textDecoration="none !important">
+			<Button size="md" w="fit-content" bg="none" border="1px solid" borderColor="brand.border_color" gap={2} p="10px 20px" _hover={{}}>
+				<Image src={`/assets/icons/${social}.svg`} />
+				{social}
+			</Button>
+		</Link>
 	)
 }
 
