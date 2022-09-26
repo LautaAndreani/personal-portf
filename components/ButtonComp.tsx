f17966d36d9a7963739ef94b7533de36e7915db7
LautaAndreani <lautaroandreani2016@gmail.com>
2022-09-26 20:56:12 -0300

    remove: :wastebasket: logs

--- components/ButtonComp.tsx
+++ components/ButtonComp.tsx
@@ -2,7 +2,6 @@ import { Button, Img, Link } from "@chakra-ui/react"
 type Props = { link: string; social: string }
 
 const ButtonComp = ({ link, social }: Props) => {
-	console.log("🚀 ~ file: ButtonComp.tsx ~ line 5 ~ ButtonComp ~ social", social)
 	return (
 		<Link isExternal href={link} fontWeight={400} textDecoration="none !important">
 			<Button size="md" w="fit-content" bg="none" border="1px solid" borderColor="brand.border_color" gap={2} p="10px 20px" _hover={{}}>
