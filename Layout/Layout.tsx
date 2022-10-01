a80c6c7aff21f04e94d6c6c63d5c616457c976cc
LautaAndreani <lautaroandreani2016@gmail.com>
2022-10-01 19:57:56 -0300

    feat: meta preview image :tada:

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children }: Props) => {
 				<title>Lautaro Andreani - frontend developer</title>
 				<link rel="icon" type="image/x-icon" href="/assets/next-profile.png" />
 				<meta property="og:title" content="Lautaro Andreani - frontend developer" />
-				{/* <meta property="og:image" content="https://res.cloudinary.com/dqp3lvyd0/image/upload/v1648516906/portfolio/Frame_1_pjbmwe.png" /> */}
+				<meta property="og:image" content="/assets/meta-header.png" />
 				<meta
 					property="og:description"
 					content="Tengo 21 años, soy de Mendoza, Argentina. Actualmente me encuentro trabajando como desarrollador frontend en..."
