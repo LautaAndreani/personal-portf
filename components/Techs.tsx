ccc491e60cf13b658eaabad05b2fb32239eb69f4
LautaAndreani <lautaroandreani2016@gmail.com>
2022-10-05 11:18:07 -0300

    styles: media queries tech badge :art:

--- components/Techs.tsx
+++ components/Techs.tsx
@@ -1,27 +1,34 @@
-import { Badge, HStack } from "@chakra-ui/react"
-import content from "../utils/content.json"
-import { Title } from "./index"
+import { Badge, HStack } from "@chakra-ui/react";
+import content from "../utils/content.json";
+import { Title } from "./index";
 
 const Techs = () => {
-	return (
-		<>
-			<Title title="Tecnologías destacadas" />
-			<HStack letterSpacing={1} flexWrap="wrap">
-				{content.mostUsedTechs.map((tech, i) => (
-					<Badge
-						key={i}
-						fontWeight={500}
-						p="8px 10px"
-						borderRadius="md"
-						variant="subtle"
-						color={`brand.color_${tech.toLowerCase()}`}
-						bg={`brand.bg_${tech.toLowerCase()}`}>
-						{tech}
-					</Badge>
-				))}
-			</HStack>
-		</>
-	)
-}
+  return (
+    <>
+      <Title title="Tecnologías destacadas" />
+      <HStack letterSpacing={1} flexWrap="wrap">
+        {content.mostUsedTechs.map((tech, i) => (
+          <Badge
+            key={i}
+            fontWeight={500}
+            p="8px 10px"
+            borderRadius="md"
+            variant="subtle"
+            color={`brand.color_${tech.toLowerCase()}`}
+            bg={`brand.bg_${tech.toLowerCase()}`}
+            m={{ sm: ".5rem .2rem !important" }}
+            sx={{
+              "@media screen and (max-width:1200px)": {
+                margin: ".5rem .2rem !important",
+              },
+            }}
+          >
+            {tech}
+          </Badge>
+        ))}
+      </HStack>
+    </>
+  );
+};
 
-export default Techs
+export default Techs;
